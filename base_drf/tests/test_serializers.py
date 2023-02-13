f32766e026d0376f53ae1858068f45f05caeb687
EvgenQr <evgen.qr@gmail.com>
2023-02-13 14:25:31 +0300

    test for drf

--- base_drf/tests/test_serializers.py
+++ base_drf/tests/test_serializers.py
@@ -1,24 +1,8 @@
 from base_drf.serializers import DocumentSerializer, DocumentDetailSerializer
-from base.models import Status, Departament, Law, Category, Document
-from django.contrib.auth.models import User
+from base.models import Status, Departament, Category
 import pytest
 from rest_framework import serializers
-
-
-def create_document():
-    user = User.objects.create(username='testuser', password='12345')
-    category = Category.objects.create(title='TestCategory')
-    departament = Departament.objects.create(title='TestDepartament')
-    status = Status.objects.create(title='TestStatus')
-    law1 = Law.objects.create(shorttitle='TestLaw')
-    law2 = Law.objects.create(shorttitle='TestLaw2')
-    document = Document.objects.create(
-        user=user,
-        title='TestDocument', category=category, departament=departament,
-        status=status)
-    document.save()
-    document.law.add(law1, law2)
-    return document
+from .create_document import create_document
 
 
 @pytest.mark.django_db
--- base_drf/urls.py
+++ base_drf/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
@@ -11,6 +15,9 @@ urlpatterns = [
     path('documents/<int:pk>/delete/', views.DocumentAPIDestroy.as_view()),
     path('auth-djoser/', include('djoser.urls')),
     re_path('auth-djoser/', include('djoser.urls.authtoken')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
